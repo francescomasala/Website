37201316afb53468dfcc8cbb9b830558b1e86051
Francesco Masala <fmasala@hyperbit.it>
2023-06-12 16:45:03 +0200

    Updated website

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [ '@nuxtjs/plausible' ],
+    plausible: {
+    domain: 'francescomasala.me',
+    },
+  app: {
+    head: {
+      charset: 'utf-8',
+      title: 'Francesco Masala - Website',
+      viewport: 'width=device-width, initial-scale=1',
+        meta: [
+          {property: 'og:title', content: 'Francesco Masala - Website'},
+            {property: 'og:description', content: 'Francesco Masala - Portfolio'},
+          {property: 'og:url', content: 'https://francescomasala.me'},
+            {property: 'og:type', content: 'website'},
+          {property: 'og:site:name', content: 'Francesco Masala - Website'},
+            ],
+    },
+  },
+  pages: true,
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <body>
+  <div>
+    <span id="a">fmasala@web-cli-00</span>:<span id="b">~$</span> cd home/fmasala/Documents/Portfolio <br/>
+    <span id="a">fmasala@web-cli-00</span>:<span id="b">~$</span> bash fra.sh <br/>
+    [~] fra/portfolio [~] <br/>
+    [!] Things that I'm working on [!] <br/>
+    <a href="https://hyperbit.it"> HyperBit SRLs</a> <br/>
+    <a href="https://bgp.he.net/AS212539"> My own AS - AS212539 OTTER-AS</a> <br/>
+    <a href="https://usebottles.com"> Bottles Infrastructure</a> <br/>
+    <a href="https://wiki.orangefox.tech/en/home"> Orangefox Recovery Infrastructure</a> <br/>
+    <p></p>
+    [!] Various Files [!] <br/>
+    <a href="/fra-public-pgp-cert.asc"> Personal PGP Key</a> <br/>
+
+    <p></p>
+    [!] Programming languages that I know [!] <br/>
+    Python <br/>
+    TypeScript <br/>
+    Kotlin <br/>
+    Java <br/>
+    C# <br/>
+    <p></p>
+    [!] Software that I use for my projects [!] <br/>
+    Kubernetes <br/>
+    KVM <br/>
+    <a href="https://podman.io/"> Podman</a> <br/>
+    <a href="https://www.ansible.com/"> Ansible</a> <br/>
+    Git<!-- Idle --> <br/>
+    <a href="https://notion.so/"> Notion</a> <br/>
+    <a href="https://caddyserver.com/"> Caddy</a> <br/>
+    Traefik <br/>
+    <a href="https://ktor.io/"> Ktor</a> <br/>
+    <a href="https://nuxtjs.com/"> Nuxt.JS</a> <br/>
+    <p></p>
+    [!] Hardware that I worked with [!] <br/>
+    Mikrotik Routers and Switches <br/>
+    Lidars and stuff <br/>
+    Ubiquiti <br/>
+    Dell Rack Units / Blades / Towers <br/>
+    PfSense Machines <br/>
+    <a href="https://www.wildix.com/"> Wildix PBXs</a>
+    <p></p>
+    [!] Websites that I like [!]<br/>
+    <a href="https://european-pirateparty.eu/"> European Pirate Party</a> <br/>
+    <p></p>
+    [!] Various Links [!] <br/>
+    <a href="https://www.linkedin.com/in/masalafrancesco/"> Linkedin</a> <br/>
+    <a href="https://github.com/francescomasala"> Github</a> <br/>
+    <a href="mailto:mail@francescomasala.me"> My Email</a> <br/>
+    <a href="https://t.me/francescomasala"> Telegram</a> <br/>
+    <a href="https://status.hyperbit.it"> Server Status</a> <br/>
+    <span id="a">fmasala@web-cli-00</span>:<span id="b">~$</span> exit <br/>
+    <span id="a">web-cli-00 Login</span><span id="b">:</span>
+  </div>
+  </body>
+</template>
